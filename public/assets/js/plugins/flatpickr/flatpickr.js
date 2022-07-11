d732471d98125bf201ed8dad4b0a3bc54feab64d
Govinda Eka Putra <govinda.ekaputra09@gmail.com>
2022-07-11 11:36:45 +0800

    update some UI and delete setupMobile function in flatpickr.js, flatpickr.min.js

--- public/assets/js/plugins/flatpickr/flatpickr.js
+++ public/assets/js/plugins/flatpickr/flatpickr.js
@@ -839,7 +839,7 @@
                 });
             }
             if (self.isMobile) {
-                setupMobile();
+                // setupMobile();
                 return;
             }
             var debouncedResize = debounce(onResize, 50);
@@ -2397,47 +2397,47 @@
                 self._input.setAttribute("readonly", "readonly");
             self._positionElement = self.config.positionElement || self._input;
         }
-        function setupMobile() {
-            var inputType = self.config.enableTime
-                ? self.config.noCalendar
-                    ? "time"
-                    : "datetime-local"
-                : "date";
-            self.mobileInput = createElement("input", self.input.className + " flatpickr-mobile");
-            self.mobileInput.tabIndex = 1;
-            self.mobileInput.type = inputType;
-            self.mobileInput.disabled = self.input.disabled;
-            self.mobileInput.required = self.input.required;
-            self.mobileInput.placeholder = self.input.placeholder;
-            self.mobileFormatStr =
-                inputType === "datetime-local"
-                    ? "Y-m-d\\TH:i:S"
-                    : inputType === "date"
-                        ? "Y-m-d"
-                        : "H:i:S";
-            if (self.selectedDates.length > 0) {
-                self.mobileInput.defaultValue = self.mobileInput.value = self.formatDate(self.selectedDates[0], self.mobileFormatStr);
-            }
-            if (self.config.minDate)
-                self.mobileInput.min = self.formatDate(self.config.minDate, "Y-m-d");
-            if (self.config.maxDate)
-                self.mobileInput.max = self.formatDate(self.config.maxDate, "Y-m-d");
-            if (self.input.getAttribute("step"))
-                self.mobileInput.step = String(self.input.getAttribute("step"));
-            self.input.type = "hidden";
-            if (self.altInput !== undefined)
-                self.altInput.type = "hidden";
-            try {
-                if (self.input.parentNode)
-                    self.input.parentNode.insertBefore(self.mobileInput, self.input.nextSibling);
-            }
-            catch (_a) { }
-            bind(self.mobileInput, "change", function (e) {
-                self.setDate(getEventTarget(e).value, false, self.mobileFormatStr);
-                triggerEvent("onChange");
-                triggerEvent("onClose");
-            });
-        }
+        // function setupMobile() {
+        //     var inputType = self.config.enableTime
+        //         ? self.config.noCalendar
+        //             ? "time"
+        //             : "datetime-local"
+        //         : "date";
+        //     self.mobileInput = createElement("input", self.input.className + " flatpickr-mobile");
+        //     self.mobileInput.tabIndex = 1;
+        //     self.mobileInput.type = inputType;
+        //     self.mobileInput.disabled = self.input.disabled;
+        //     self.mobileInput.required = self.input.required;
+        //     self.mobileInput.placeholder = self.input.placeholder;
+        //     self.mobileFormatStr =
+        //         inputType === "datetime-local"
+        //             ? "Y-m-d\\TH:i:S"
+        //             : inputType === "date"
+        //                 ? "Y-m-d"
+        //                 : "H:i:S";
+        //     if (self.selectedDates.length > 0) {
+        //         self.mobileInput.defaultValue = self.mobileInput.value = self.formatDate(self.selectedDates[0], self.mobileFormatStr);
+        //     }
+        //     if (self.config.minDate)
+        //         self.mobileInput.min = self.formatDate(self.config.minDate, "Y-m-d");
+        //     if (self.config.maxDate)
+        //         self.mobileInput.max = self.formatDate(self.config.maxDate, "Y-m-d");
+        //     if (self.input.getAttribute("step"))
+        //         self.mobileInput.step = String(self.input.getAttribute("step"));
+        //     self.input.type = "hidden";
+        //     if (self.altInput !== undefined)
+        //         self.altInput.type = "hidden";
+        //     try {
+        //         if (self.input.parentNode)
+        //             self.input.parentNode.insertBefore(self.mobileInput, self.input.nextSibling);
+        //     }
+        //     catch (_a) { }
+        //     bind(self.mobileInput, "change", function (e) {
+        //         self.setDate(getEventTarget(e).value, false, self.mobileFormatStr);
+        //         triggerEvent("onChange");
+        //         triggerEvent("onClose");
+        //     });
+        // }
         function toggle(e) {
             if (self.isOpen === true)
                 return self.close();
