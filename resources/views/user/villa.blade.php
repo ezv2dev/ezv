ef14bc50e7c9dff5e77c67d17b4a786ad1db7b9c
Angga Rahayudiputra <anggarahayudiputra@gmail.com>
2022-07-11 17:07:37 +0800

    update

--- resources/views/user/villa.blade.php
+++ resources/views/user/villa.blade.php
@@ -1172,7 +1172,7 @@
                                     <input class="guest-right-input" type="number" id="total_guest2" value="1"
                                         min="0" readonly>
                                 </button>
-                                <div class="content sidebar-popup" id="popup_guest">
+                                <div class="content sidebar-popup-tamu" id="popup_guest">
                                     <div class="row" style="margin-top: 10px;">
 
                                         <div class="reserve-input-row">
