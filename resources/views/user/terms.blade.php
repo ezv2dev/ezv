6ae9ddfe0cb5d4cc9db418110e75e1e4b1cd2399
dewaraija <74578814+dewaraija@users.noreply.github.com>
2022-07-22 17:09:48 +0800

    Update terms.blade.php
    Update

--- resources/views/user/terms.blade.php
+++ resources/views/user/terms.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Collaborator Program in Brief - EZV</title>
+    <title>Terms & Conditions - EZV</title>
     <meta name="description" content="EZV2 created by pixelcave and published on Themeforest">
     <meta name="author" content="pixelcave">
     <meta name="robots" content="noindex, nofollow">
