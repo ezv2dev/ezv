af3075940a1f023ce38effd325ff8d305b4352f8
febrisa14 <gdefebrisapermana@gmail.com>
2022-07-23 10:25:44 +0800

    swap position photo in wow detail

--- resources/views/user/activity/activity.blade.php
+++ resources/views/user/activity/activity.blade.php
@@ -4673,6 +4673,11 @@
         }
         // Save order
         function save_reorder_photo() {
+
+            let btn = document.getElementById("saveBtnReorderPhoto");
+            btn.textContent = "Saving...";
+            btn.classList.add("disabled");
+
             var imageids_arr = [];
             // get image ids order
             $('#sortable-photo li').each(function() {
@@ -4689,7 +4694,63 @@
                     id: `{{ $activity->id_activity }}`
                 },
                 success: function(response) {
-                    location.reload();
+                    console.log(response);
+
+                    iziToast.success({
+                        title: "Success",
+                        message: response.message,
+                        position: "topRight",
+                    });
+
+                    let path = "/foto/activity/";
+                    let slash = "/";
+                    let uid = response.data.uid.uid;
+                    let lowerCaseUid = uid.toLowerCase();
+                    let content = "";
+                    let contentPositionModal = "";
+
+                    for (let i = 0; i < response.data.photo.length; i++) {
+                        content += '<div class="col-4 grid-photo" id="displayPhoto'+
+                            response.data.photo[i].id_photo+
+                            '"> <a href="'+
+                            path+lowerCaseUid+slash+response.data.photo[i].name+
+                            '"> <img class="photo-grid img-lightbox lozad-gallery-load lozad-gallery" src="'+
+                            path+lowerCaseUid+slash+response.data.photo[i].name+
+                            '"> </a> <span class="edit-icon"> <button data-bs-toggle="popover" data-bs-animation="true" data-bs-placement="bottom" type="button" title="{{ __('user_page.Add Photo Tag') }}" data-id="{{ $activity->id_activity }}" data-photo="'+
+                            response.data.photo[i].id_photo+
+                            '" onclick="add_photo_tag(this)"><i class="fa fa-pencil"></i></button> <button data-bs-toggle="popover" data-bs-animation="true" data-bs-placement="bottom" title="{{ __('user_page.Swap Photo Position') }}" type="button" onclick="position_photo()"><i class="fa fa-arrows"></i></button> <button data-bs-toggle="popover" data-bs-animation="true" data-bs-placement="bottom" title="{{ __('user_page.Delete Photo') }}" href="javascript:void(0);" data-id="{{ $activity->id_activity }}" data-photo="'+
+                            response.data.photo[i].id_photo+
+                            '" onclick="delete_photo_photo(this)"><i class="fa fa-trash"></i></button> </span> </div>';
+
+                        contentPositionModal += '<li class="ui-state-default" data-id="' + response.data.photo[i]
+                            .id_photo + '" id="positionPhotoGallery' + response.data.photo[i].id_photo +
+                            '"> <img src="' +
+                            path + lowerCaseUid + slash + response.data.photo[i].name +
+                            '" title="' + response.data.photo[i].name + '"> </li>';
+
+                    }
+                    if (response.data.video.length > 0)
+                    {
+                        for (let v = 0; v < response.data.video.length; v++) {
+                            content += '<div class="col-4 grid-photo" id="displayVideo'+response.data.video[v].id_video+'"> <a class="pointer-normal" onclick="view_video_activity('+
+                                response.data.video[v].id_video+')" href="javascript:void(0);"> <video href="javascript:void(0)" class="photo-grid" loading="lazy" src="'+
+                                path+lowerCaseUid+slash+response.data.video[v].name+
+                                '#t=1.0"></video> <span class="video-grid-button"><i class="fa fa-play"></i></span> </a> <span class="edit-video-icon"> <button type="button" onclick="position_video()" data-bs-toggle="popover" data-bs-animation="true" data-bs-placement="bottom" title="{{ __('user_page.Swap Video Position') }}"><i class="fa fa-arrows"></i></button> <button href="javascript:void(0);" data-id="{{ $activity->id_activity }}" data-video="'+
+                                response.data.video[v].id_video+'" onclick="delete_photo_video(this)" data-bs-toggle="popover" data-bs-animation="true" data-bs-placement="bottom" title="{{ __('user_page.Delete Video') }}"><i class="fa fa-trash"></i></button> </span> </div>';
+                        }
+                    }
+                        
+                    btn.textContent = "{{ __('user_page.Save') }}";
+                    btn.classList.remove("disabled");
+
+                    $('.gallery').html("");
+                    $('.gallery').append(content);
+                    $('#sortable-photo').html("");
+                    $('#sortable-photo').append(contentPositionModal);
+
+                    $("#edit_position_photo").modal("hide");
+
+                    $gallery.refresh();
                 }
             });
         }
