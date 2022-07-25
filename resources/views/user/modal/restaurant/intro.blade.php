aedd568417ea4f7536b88e9de7951566a224d507
agusmahendraa <creativemahen@github.com>
2022-07-25 13:15:12 +0800

    update

--- resources/views/user/modal/restaurant/intro.blade.php
+++ resources/views/user/modal/restaurant/intro.blade.php
@@ -1,7 +1,7 @@
 <!-- Fade In Default Modal -->
 <div class="modal fade" id="introModal" tabindex="-1" role="dialog" aria-labelledby="modal-default-fadein"
     aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-fullscreen-md-down modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Introduction to Food Page</h5>
