c9c3397b6b46d773328cb298afe423c059cb3ebb
dewaraija <74578814+dewaraija@users.noreply.github.com>
2022-07-27 11:51:34 +0800

    Update
    Update

--- resources/views/user/about_us.blade.php
+++ resources/views/user/about_us.blade.php
@@ -97,8 +97,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
-
     <script>
         //Sticky Bar
         $(function(){
@@ -112,30 +110,8 @@
             });
         });
     </script>
-
     <script src="{{ asset('assets/js/dashmix.app.min.js') }}"></script>
-
-    <!-- jQuery (required for Magnific Popup plugin) -->
     <script src="{{ asset('assets/js/lib/jquery.min.js') }}"></script>
-
-    <!-- Page JS Plugins -->
-    <script src="{{ asset('assets/js/plugins/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
-
-    <script src="{{ asset('assets/js/view-villa.js') }}"></script>
-
-    <!-- Page JS Helpers (Magnific Popup Plugin) -->
-    <script>
-        Dashmix.helpersOnLoad(['jq-slick']);
-
-    </script>
-
-    <!-- Page JS Helpers (Magnific Popup Plugin) -->
-    <script>
-        Dashmix.helpersOnLoad(['jq-magnific-popup']);
-
-    </script>
-
-    <!-- Tambahan -->
     <script src="{{ asset('assets/js/home.js') }}"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
