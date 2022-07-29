c0d6c79f29715a6bf357d91ca6bc4aafa495fc2b
agusmahendraa <creativemahen@github.com>
2022-07-29 15:04:16 +0800

    update

--- resources/views/user/modal/restaurant/tag_add.blade.php
+++ resources/views/user/modal/restaurant/tag_add.blade.php
@@ -69,7 +69,7 @@
 
 <div class="modal fade" id="modal-add_tag" tabindex="-1" role="dialog" aria-labelledby="modal-default-fadein"
     aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-fullscreen-md-down modal-dialog-centered modal-lg" role="document">
         <div class="modal-content" style="height: 550px;">
             <div class="modal-header" style="padding-left: 2.3rem !important;">
                 <h5 class="modal-title">{{ __('user_page.Edit Subcategory') }}</h5>
