decc88af44828502e2ff6052787b6ce5f3fcb3d8
anggakusumap <37077181+anggakusumap@users.noreply.github.com>
2022-08-02 11:10:20 +0800

    popular-villa-cookie-date-wow-and-restaurant

--- resources/views/user/restaurant/restaurant.blade.php
+++ resources/views/user/restaurant/restaurant.blade.php
@@ -81,6 +81,25 @@
         $condition_restaurant = Route::is('restaurant');
         $condition_hotel = Route::is('hotel') || Route::is('room_hotel');
         $condition_things_to_do = Route::is('activity') || Route::is('activity_price_index');
+
+        if (isset($_COOKIE['sCheck_in'])) {
+            $get_check_in = $_COOKIE['sCheck_in'];
+            $get_check_out = $_COOKIE['sCheck_out'];
+        } else {
+            $get_check_in = null;
+            $get_check_out = null;
+        }
+
+        function dateDiff($get_check_in, $get_check_out)
+        {
+            $date1_ts = strtotime($get_check_in);
+            $date2_ts = strtotime($get_check_out);
+            $datediff = $date2_ts - $date1_ts;
+
+            return round($datediff / (60 * 60 * 24));
+        }
+
+        $dateDiffe = dateDiff($get_check_in, $get_check_out);
     @endphp
 
     @include('components.notification.notification')
@@ -566,7 +585,7 @@
                                     <div id="time-form-mobile" style="display:none;">
                                         <form action="{{ route('restaurant_update_time') }}" method="post">
                                             <!-- @csrf
-                                                                                                                                                        @method('PATCH') -->
+                                                                                                                                                                                                                    @method('PATCH') -->
                                             <input type="hidden" name="id_restaurant"
                                                 value="{{ $restaurant->id_restaurant }}" required>
                                             <div class="form-group d-flex justify-content-start align-items-center">
@@ -1365,34 +1384,39 @@
                                 @endauth
                             </h2>
                             @php
-                                $isMobile = preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$_SERVER['HTTP_USER_AGENT'])||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($_SERVER['HTTP_USER_AGENT'],0,4));
+                                $isMobile =
+                                    preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $_SERVER['HTTP_USER_AGENT']) ||
+                                    preg_match(
+                                        '/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',
+                                        substr($_SERVER['HTTP_USER_AGENT'], 0, 4),
+                                    );
                             @endphp
                             <p id="description-content"
                                 style="text-align: justify; padding-top:10px; padding-bottom:12px">
                                 @if ($isMobile)
-                                {!! Str::limit(Translate::translate($restaurant->description), 400, ' ...') ??
-                                __('user_page.There is no description yet') !!}
+                                    {!! Str::limit(Translate::translate($restaurant->description), 400, ' ...') ??
+                                        __('user_page.There is no description yet') !!}
                                 @else
-                                {!! Str::limit(Translate::translate($restaurant->description), 600, ' ...') ??
-                                    __('user_page.There is no description yet') !!}
+                                    {!! Str::limit(Translate::translate($restaurant->description), 600, ' ...') ??
+                                        __('user_page.There is no description yet') !!}
                                 @endif
                                 {{-- {!! $restaurant->description ?? 'there is no description yet' !!} --}}
                             </p>
                             <span id="buttonShowMoreDescription">
                                 @if ($isMobile)
-                                @if (Str::length($restaurant->description) > 400)
-                                <a id="btnShowMoreDescription" style="font-weight: 600;"
-                                    href="javascript:void(0);" onclick="showMoreDescription();"><span
-                                        style="text-decoration: underline; color: #ff7400;">{{ __('user_page.Show more') }}</span>
-                                    <span style="color: #ff7400;">></span></a>
-                                @endif
+                                    @if (Str::length($restaurant->description) > 400)
+                                        <a id="btnShowMoreDescription" style="font-weight: 600;"
+                                            href="javascript:void(0);" onclick="showMoreDescription();"><span
+                                                style="text-decoration: underline; color: #ff7400;">{{ __('user_page.Show more') }}</span>
+                                            <span style="color: #ff7400;">></span></a>
+                                    @endif
                                 @else
-                                @if (Str::length($restaurant->description) > 600)
-                                    <a id="btnShowMoreDescription" style="font-weight: 600;"
-                                        href="javascript:void(0);" onclick="showMoreDescription();"><span
-                                            style="text-decoration: underline; color: #ff7400;">{{ __('user_page.Show more') }}</span>
-                                        <span style="color: #ff7400;">></span></a>
-                                @endif
+                                    @if (Str::length($restaurant->description) > 600)
+                                        <a id="btnShowMoreDescription" style="font-weight: 600;"
+                                            href="javascript:void(0);" onclick="showMoreDescription();"><span
+                                                style="text-decoration: underline; color: #ff7400;">{{ __('user_page.Show more') }}</span>
+                                            <span style="color: #ff7400;">></span></a>
+                                    @endif
                                 @endif
                             </span>
                             @auth
@@ -1581,8 +1605,19 @@
                                                         </div>
                                                         <div class="popular-card-price">
                                                             @if (!empty($popular->price))
-                                                                <p>{{ CurrencyConversion::exchangeWithUnit($popular->price) }}/{{ __('user_page.night') }}
-                                                                </p>
+                                                                @if (isset($_COOKIE['sCheck_in']) && isset($_COOKIE['sCheck_out']))
+                                                                    <p>{{ CurrencyConversion::exchangeWithUnit($popular->price) }}/
+                                                                        {{ $dateDiffe }}
+                                                                        {{ __('user_page.night') }}
+                                                                        <br>
+                                                                        <b>{{ $get_check_in }}</b> to
+                                                                        <b>{{ $get_check_out }}</b>
+                                                                    </p>
+                                                                @else
+                                                                    <p>{{ CurrencyConversion::exchangeWithUnit($popular->price) }}/
+                                                                        {{ __('user_page.night') }}
+                                                                    </p>
+                                                                @endif
                                                             @endIf
                                                         </div>
                                                     </div>
@@ -1861,7 +1896,8 @@
                                         </div>
                                     </div>
                                     <div class="col-12 pt-3">
-                                        <button type="button" onclick="showMoreReview();" class="btn btn-outline-dark">
+                                        <button type="button" onclick="showMoreReview();"
+                                            class="btn btn-outline-dark">
                                             Show all reviews
                                         </button>
                                     </div>
